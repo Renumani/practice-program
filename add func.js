f136c28cc7ceed7a8e2a992ccd8d858aa9bae7fa
Renumani <87789647+Renumani@users.noreply.github.com>
2021-07-30 22:49:01 +0530

    Add files via upload

---
+++ add func.js
@@ -0,0 +1,9 @@
+function sum(num1,num2)
+	{
+		var z = num1+num2;
+		return z;
+	}
+var s = sum(5,7);
+console.log("the sum is:"+ s);
+let  add = (x,y) => x+y;
+console.log(add(10,20));
---
+++ pattern.js
@@ -0,0 +1,10 @@
+let row = prompt("enter the number");
+for(var i=1;i<=row;i++)
+{
+    let str =" ";
+    for(var j=1;j<=i;j++)
+    {
+        str+="*";
+    }
+    console.log(str);
+}
