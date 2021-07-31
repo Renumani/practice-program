ad286bac5aef95bd3231ae5f564cffd16c2c3539
Renumani <87789647+Renumani@users.noreply.github.com>
2021-07-31 19:26:55 +0530

    first commit

---
+++ expoadd func.js
@@ -0,0 +1,14 @@
+function sum(num1,num2)
+	{
+		var z = num1+num2;
+		return z;
+	}
+var s = sum(5,7);
+console.log("the sum is:"+ s);
+function power(s,n)
+{
+    var y = s**n;
+    return y;
+}
+var e = power(s,2);
+console.log("the exponent of"+ s +"is:" + e);
