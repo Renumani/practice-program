d1c7c4919cd0ccfa9c7c3f63cf19975e7f0b4a55
Renumani <87789647+Renumani@users.noreply.github.com>
2021-08-20 21:45:05 +0530

    Add files via upload

---
+++ funcarro.js
@@ -0,0 +1,16 @@
+{// single line arrow function
+let hours = (x)=>{
+    return x*60;}
+let years = (y)=>{
+    return y*365;
+}
+console.log(hours(8));
+console.log(years(22));
+
+//multi line a}rrow function
+let add = (z)=>{
+    let y = [z*(z+1)]/2;
+	return y;
+    document.write(y);
+}  
+console.log(add(5));
