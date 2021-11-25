c94212b6cb61e61ec7d1ff703587275ff2495137
Renumani <87789647+Renumani@users.noreply.github.com>
2021-11-25 13:13:35 +0530

    Added factorial program

--- func.py
+++ func.py
@@ -1,5 +1,19 @@
 def sum(x,y):
     z=x+y;
-    return z;
-    print('the sum of two number is:'+ z);
-sum(5,8);
+    return 
+x=int(input('enter the 1st number:');
+y=int(input('enter the 2nd number:');
+print('the sum of two number is:'+str(sum(x,y)));
+
+ #factorial
+ def factorial(num):
+    answer=1;
+    if(num==0 or num==1):
+      return 1;
+    else: 
+     for i in range(1,num):
+       answer=answer*num;
+       num=num-1;
+    return answer;
+num = int(input('enter the number:'));
+print('the factorial of '+str(num)+ ' is:'+str(factorial(num)));
